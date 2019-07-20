ea720ce3b21bd00fc4ac15dbae72a076a31b3a6f
Amit G <amit.gardharia@gmail.com>
2019-07-20 13:00:04 -0400

    Update emp_setup.sql

--- emp_setup.sql
+++ emp_setup.sql
@@ -24,13 +24,13 @@ select date'2018-09-01', 1, 'a@test.com', 'full', date'2018-12-31' from dual
 union all
 select date'2018-01-01', 2, '',           'temp', date'2018-12-31' from dual
 union all
-select date'2018-02-01', 2, 'a@test.com', 'temp', date'2018-12-31' from dual
+select date'2018-02-01', 2, 'b@test.com', 'temp', date'2018-12-31' from dual
 union all
-select date'2018-03-01', 2, 'a@test.com', 'temp', date'2018-04-30' from dual
+select date'2018-03-01', 2, 'b@test.com', 'temp', date'2018-04-30' from dual
 union all
-select date'2018-04-01', 2, 'a@test.com', 'temp', date'2018-04-30' from dual
+select date'2018-04-01', 2, 'b@test.com', 'temp', date'2018-04-30' from dual
 union all
-select date'2018-07-01', 2, 'a@test.com', 'full', date'2018-07-31' from dual
+select date'2018-07-01', 2, 'b@test.com', 'full', date'2018-07-31' from dual
 union all
-select date'2018-09-01', 2, 'a@test.com', 'full', date'2018-12-31' from dual
+select date'2018-09-01', 2, 'b@test.com', 'full', date'2018-12-31' from dual
 
